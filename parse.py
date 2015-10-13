33ccc94f792639b1146b60de3e6fec067f099df4
Tim Monks <t.r.monks@gmail.com>
2015-10-13 09:09:04 -0700

    start work on user provided safe interpolation strings

---
+++ parse.py
@@ -0,0 +1,43 @@
+import re
+from StringIO import StringIO
+
+# eat everything up to first unescaped {
+# /.*?[^\\]{/
+# eat everything up to first }
+# /.*?}/
+# repeat until empty
+
+BEFORE_INTERP = re.compile(r'((?:.*?)[^\\]|)\{')
+IN_INTERP = re.compile(r'\{(.*?)\}')
+
+def execute(interp_string, parse_context):
+    result = StringIO()
+    while interp_string:
+        match = BEFORE_INTERP.match(interp_string)
+        if match is not None:
+            result.write(match.groups()[0])
+            interp_string = interp_string[len(match.groups()[0]):]
+        else:
+            # no interpolation left
+            result.write(interp_string)
+            interp_string = ''
+            next
+
+        inside_match = IN_INTERP.match(interp_string)
+        if inside_match is not None:
+            result.write(str(getattr(parse_context, inside_match.groups()[0])))
+            interp_string = interp_string[len(inside_match.groups()[0]) + 2:]
+        else:
+            # only broken interpolation left
+            result.write(interp_string)
+            interp_string = ''
+            next
+
+    return result.getvalue()
+
+class ParseContext(object):
+    def __init__(self, **data):
+        self._data = data
+
+    def __getattr__(self, k):
+        return self._data[k]
---
+++ test_parse.py
@@ -0,0 +1,15 @@
+import parse
+
+import hamcrest as h
+
+def test_parse_context():
+    h.assert_that(parse.ParseContext(foo=2).foo, h.equal_to(2))
+
+def test_no_interp():
+    h.assert_that(parse.execute('foo', None), h.equal_to('foo'))
+
+def test_interp_at_start():
+    h.assert_that(parse.execute('{foo}', parse.ParseContext(foo=2)), h.equal_to('2'))
+
+def test_unclosed():
+    h.assert_that(parse.execute('{foo', parse.ParseContext(foo=2)), h.equal_to('{foo'))
